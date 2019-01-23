beb0dc65d3abf3b9763f6f8be48ae99ceb759b53
Wendell Alves <wendellmccaulay@hotmail.com>
2019-01-23 08:12:16 -0300

    Apliquei autocomplete ao form do admin :bowtie:

--- buscacep/admin.py
+++ buscacep/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Endereco
+from .forms import EnderecoForm
 
 # Register your models here.
+class EnderecoAdmin(admin.ModelAdmin):
+    form = EnderecoForm
+    # class Media:
+    #     js = ('js/jquery-3.3.1.min.js', 'js/script-cep.js',)
+
+admin.site.register(Endereco, EnderecoAdmin)
