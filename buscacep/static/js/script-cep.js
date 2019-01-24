76a41a4edebabe5fda0c9d884e1a70af408258a1
Breno Ferreira <breno.leite@parnamirim.rn.gov.br>
2019-01-24 13:54:40 -0300

    Adição de função para remover acentos do JSON e alteração no .gitignore

--- buscacep/static/js/script-cep.js
+++ buscacep/static/js/script-cep.js
@@ -5,12 +5,25 @@ $(document).ready(function () {
         if (cep.length == 9) {
             $.getJSON("http://viacep.com.br/ws/" + cep + "/json/ ", function (data) {
                 var endereco = data;
-                var localidade = endereco.localidade;
+                var localidade = endereco.localidade.toUpperCase()
                 var logradouro = endereco.logradouro;
                 var bairro = endereco.bairro;
                 var uf = endereco.uf;
+                function RemoveAccents(str) {
+                  var accents    = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+                  var accentsOut = "AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz";
+                  str = str.split('');
+                  var strLen = str.length;
+                  var i, x;
+                  for (i = 0; i < strLen; i++) {
+                    if ((x = accents.indexOf(str[i])) != -1) {
+                      str[i] = accentsOut[x];
+                    }
+                  }
+                  return str.join('');
+                }
 
-                $('#id_localidade').val(localidade);
+                $('#id_localidade').val(RemoveAccents(localidade));
                 $("#id_logradouro").val(logradouro);
                 $("#id_bairro").val(bairro);
                 $("#id_uf").val(uf);
